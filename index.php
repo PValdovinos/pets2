8ec9b80ddfadf4e783fd8b431cbf7e9e9abfd587
Pedro V.R. <ptreyes2@gmail.com>
2024-04-18 14:36:19 -0400

    Added order-summary and fleshed out and completed it

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+//session_start();
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -27,9 +27,38 @@ $f3->route('GET|POST /order', function($f3){
 
     //echo '<h1>Order Page</h1>';
 
+    // Check if the form has been posted
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+
+        // Get the data
+        $pet = $_POST['pet'];
+        $color = $_POST['color'];
+
+        // Validate the data
+        if(!empty('pet') && ($color != 'No'))
+        {
+            // Data is invalid
+            echo "Please supply a pet type";
+
+            // Data is valid
+            $f3->set('SESSION.pet', $pet);
+
+            $f3->set('SESSION.color', $color);
+
+            $f3->reroute('summary');
+        }
+    }
+
     //render a view page
     $view = new Template();
     echo $view->render('views/pet-order.html');
+});
+
+$f3->route('GET|POST /summary', function($f3){
+
+    $view = new Template();
+    echo $view->render('views/order-summary.html');
 });
 
 // Run Fat-Free
